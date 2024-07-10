a49ce7dbfe919e5cf3c7f6e73495081f4b339a78
raxvandy <90607941+raxvandy@users.noreply.github.com>
2024-07-10 04:35:34 -0700

    Update testspeed.sh

--- testspeed.sh
+++ testspeed.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
-for i in {9..20}
-do
-bs=$((2**i))
-dd bs="$bs" if=/dev/zero of=/dev/null status=progress |& { mapfile -d$'\r' -n 2; printf '%-8s : %s\n' "$bs" "${MAPFILE[1]}"; }
-done
-exit
+#for i in {9...20}
+#do
+#bs=$((2**i))
+#dd bs="$bs" if=/dev/zero of=/dev/null status=progress |& { mapfile -d$'\r' -n 2; printf '%-8s : %s\n' "$bs" "${MAPFILE[1]}"; }
+#done
+#exit
